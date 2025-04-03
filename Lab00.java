280426bfc7580b5cb22b035072c2600a7f3338bd
aarav <anaray07@calpoly.edu>
2025-04-03 10:17:27 -0700

    lab00

--- Lab00.java
+++ Lab00.java
@@ -1,8 +1,33 @@
+import java.util.Objects;
+
 public class Lab00
 {
    public static void main(String[] args)
    {
+      int x = 5;
+      String y = "hello";
+      double z = 9.8;
+      System.out.println("x: "+x+" y: "+y+" z: "+z);
+      int[] nums={3,6,-1,2};
+      for (int i=0; i<nums.length;i++){
+         System.out.println(nums[i]);
+      }
 
-   
+      int numFound=char_count(y,'l');
+      System.out.println("Found: "+numFound);
+      for (int i=1; i<11;i++){
+         System.out.print(i+" ");
+      }
+      System.out.println();
+   }
+   public static int char_count(String s, char c){
+      int count=0;
+      int ch;
+      for (ch=0;ch<s.length();ch++){
+         if (s.charAt(ch)==c){
+            count+=1;
+         }
+      }
+      return count;
    }
 }
